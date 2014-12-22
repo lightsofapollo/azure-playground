27347c894ed8e2e9cd91572a87d36f6594f7199f
James Lal <james@lightsofapollo.com>
2014-12-21 18:42:30 -0800

    do bad things

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHead(200);
-  res.write('woot');
+  res.write(JSON.stringify(process.env));
   res.end();
 }).listen(process.env.PORT || 80);
