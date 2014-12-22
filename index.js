72c5bf9f38607959a3e8051745e3400edce63f14
James Lal <james@lightsofapollo.com>
2014-12-21 23:42:28 -0800

    correct the port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var hapi = require('hapi');
 
 var server = new hapi.Server();
-server.connection({ port: 60023 });
+server.connection({ port: process.env.PORT || 80 });
 
 server.start(function() {
   console.log('doing stuff yay', server.info.uri);
