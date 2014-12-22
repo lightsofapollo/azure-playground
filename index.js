c91ed45ccee96e745409049822eeffb5ed007547
James Lal <james@lightsofapollo.com>
2014-12-21 18:45:34 -0800

    add debug module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var debug = require('debug')('azure deubg');
 
 http.createServer(function(req, res) {
+  debug('made request');
   res.writeHead(200);
   res.write(JSON.stringify(process.env));
   res.end();
